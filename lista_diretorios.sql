0b2b44d69cf97d98ccea66ce01e80a35af17253d
Pittsauro <76506820+Pittsauro@users.noreply.github.com>
2021-03-14 19:36:13 -0300

    Update lista_diretorios.sql

--- lista_diretorios.sql
+++ lista_diretorios.sql
@@ -3,7 +3,7 @@ set head off;
 set feedback off;
 set echo off;
 set verify off trimspool on
-spool /home/oracle/automacao/diretorios.txt
+spool /home/oracle/trilha_devops/diretorios.txt
 SELECT DIRECTORY_NAME FROM DBA_DIRECTORIES ORDER BY DIRECTORY_NAME;
 spool off;
 exit
