40bbd8ba3123fddb373661d8a021bdde2ed04d6e
Pittsauro <76506820+Pittsauro@users.noreply.github.com>
2021-03-14 19:33:14 -0300

    Update instances_running.sh

--- instances_running.sh
+++ instances_running.sh
@@ -1,1 +1,1 @@
-ps -ef | grep pmon | grep -v grep | grep -v +ASM | awk '{ print $8 }' | cut -d '_' -f3 > /home/oracle/automacao/instances_running.txt
+ps -ef | grep pmon | grep -v grep | grep -v +ASM | awk '{ print $8 }' | cut -d '_' -f3 > /home/oracle/trilha_devops/instances_running.txt
